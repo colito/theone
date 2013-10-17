1ae5d55be92cf30b81de17be542af108c8abffdc
Pride Mokhele <pride.mokhele@gmail.com>
2013-10-17 10:07:54 +0200

    * first commit

--- include_scripts/slider.php
+++ include_scripts/slider.php
@@ -4,13 +4,13 @@
         <div id='picshow' class='grid_12 flexslider omega'>
 			<ul class='slides'>
 				<li>
-					<img src='../images/1.jpg' />
+					<img src='images/1.jpg' />
 				</li>
 				<li>
-					<img src='../images/2.jpg' />
+					<img src='images/2.jpg' />
 				</li>
 				<li>
-					<img src='../images/3.jpg' />
+					<img src='images/3.jpg' />
 				</li>
 			</ul>
 		</div>
