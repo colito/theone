11252df82506f72592caadccf09ca248c260fc17
Pride Mokhele <pride.mokhele@gmail.com>
2013-10-17 10:17:51 +0200

    * changed the site name

--- include_scripts/topper.php
+++ include_scripts/topper.php
@@ -12,12 +12,12 @@
         <div id='topper'>
 
             <div id='title'>
-                <h1><a href='index.php'>Titulo</a></h1>
+                <h1><a href='index.php'>Success Motivation</a></h1>
             </div>
 
             <div id='navigation'>
                 <ul>
-                    <li id=$home_active_page class='home'><a href='../rev-6'>Home</a></li>
+                    <li id=$home_active_page class='home'><a href='index.php'>Home</a></li>
                     <li id=$solutions_active_page class='solutions'><a href='solutions.php'>Solutions</a></li>
                     <li id=$contact_active_page class='contact'><a href='contact.php'>Contact Us</a></li>
                 </ul>
@@ -28,7 +28,7 @@
         <div id='switch'>menu</div>
         <div id='panel'>
             <ul>
-				<li class='home'><a href='../rev-6'>Home</a></li>
+				<li class='home'><a href='index.php'>Home</a></li>
 				<li class='solutions'><a href='solutions.php'>Solutions</a></li>
                 <li class='contact'><a href='contact.php'>Contact Us</a></li>
 			</ul>
