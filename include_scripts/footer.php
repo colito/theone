c5b73ab452c2e2d0f9d3790c0b903be3043f1031
Pride Mokhele <pride.mokhele@gmail.com>
2013-10-17 10:16:33 +0200

    * removed audio player at footer

--- include_scripts/footer.php
+++ include_scripts/footer.php
@@ -5,14 +5,6 @@
         <div id='footer' class='grid_12'>
 			<div id='copyright'>
 				<p>&copy; 2013 Success Motivation. All Rights Reserved</p>
-				<p>
-				     <audio controls height='50' width='50'>
-                        <source src='take_five.mp3' type='audio/mpeg'>
-                        <source src='autumn_leaves.mp3' type='audio/mpeg'>
-
-                        Your browser does not support this audio format.
-                    </audio>
-				</p>
 			</div>
 		</div>
     ";
