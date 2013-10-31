4bc68db0ac8573844d2ebae9044f6ad863bbb4fc
Pride Mokhele <pride.mokhele@gmail.com>
2013-10-31 15:56:10 +0200

    * upgraded footer.php to meet the templata standard

--- include_scripts/footer.php
+++ include_scripts/footer.php
@@ -1,12 +1,6 @@
-<?php
-
-    echo "
-
-        <div id='footer' class='grid_12'>
-			<div id='copyright'>
-				<p>&copy; 2013 Success Motivation. All Rights Reserved</p>
-			</div>
-		</div>
-    ";
-
-?>
+<div id='footer' class='grid_12'>
+    <div id='copyright'>
+        <p>&copy; 2013 Success Motivation. All Rights Reserved</p>
+    </div>
+</div>
+<?php ob_flush();?>
