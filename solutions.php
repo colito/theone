8240780f296c3e637d718a341fccc99b27b4b6a9
Pride Mokhele <pride.mokhele@gmail.com>
2013-11-01 12:56:42 +0200

    * updated solutions page to templata standard

--- solutions.php
+++ solutions.php
@@ -1,77 +1,61 @@
-<html lang="en">
-    <head>
+<?php $page_name = 'Solutions'; ?>
 
-        <title>Solutions</title>
-        <?php require_once('include_scripts/uniform.php'); $solutions_active_page = 'active_page'; ?>
-
-    </head>
-
-    <?php require_once('include_scripts/topper.php'); ?>
-
-    <body>
-
-        <div class='container clearfix'>
-
-            <?php require_once('include_scripts/header.php'); ?>
+<?php require_once('includes/header.php'); ?>
 
 
-            <div id='content-card' class='grid_5'>
-                <p>
-                    1 Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi.
-                    Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa. Sed eleifend nonummy diam.
-                    Praesent mauris ante, elementum et, bibendum at, posuere sit amet, nibh. Duis tincidunt lectus quis dui viverra vestibulum.
-                    Suspendisse vulputate aliquam dui. Nulla elementum dui ut augue. Aliquam vehicula mi at mauris.
-                    Maecenas placerat, nisl at consequat rhoncus, sem nunc gravida justo, quis eleifend arcu velit quis lacus.
-                    Morbi magna magna, tincidunt a, mattis non, imperdiet vitae, tellus.
-                    Sed odio est, auctor ac, sollicitudin in, consequat vitae, orci.
-                    Fusce id felis. Vivamus sollicitudin metus eget eros.
-                </p>
+<div id='content-card' class='grid_5'>
+    <p>
+        1 Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi.
+        Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa. Sed eleifend nonummy diam.
+        Praesent mauris ante, elementum et, bibendum at, posuere sit amet, nibh. Duis tincidunt lectus quis dui viverra vestibulum.
+        Suspendisse vulputate aliquam dui. Nulla elementum dui ut augue. Aliquam vehicula mi at mauris.
+        Maecenas placerat, nisl at consequat rhoncus, sem nunc gravida justo, quis eleifend arcu velit quis lacus.
+        Morbi magna magna, tincidunt a, mattis non, imperdiet vitae, tellus.
+        Sed odio est, auctor ac, sollicitudin in, consequat vitae, orci.
+        Fusce id felis. Vivamus sollicitudin metus eget eros.
+    </p>
 
-            </div>
+</div>
 
-            <div id='content-card1' class='grid_5'>
+<div id='content-card1' class='grid_5'>
 
-                <p>
-                    2.1 Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi.
-                    Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa. Sed eleifend nonummy diam.
-                    Praesent mauris ante, elementum et, bibendum at, posuere sit amet, nibh. Duis tincidunt lectus quis dui viverra vestibulum.
-                    Suspendisse vulputate aliquam dui. Nulla elementum dui ut augue. Aliquam vehicula mi at mauris.
-                    Maecenas placerat, nisl at consequat rhoncus, sem nunc gravida justo, quis eleifend arcu velit quis lacus.
-                    Morbi magna magna, tincidunt a, mattis non, imperdiet vitae, tellus.
-                    Sed odio est, auctor ac, sollicitudin in, consequat vitae, orci.
-                    Fusce id felis. Vivamus sollicitudin metus eget eros.
-                </p>
+    <p>
+        2.1 Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi.
+        Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa. Sed eleifend nonummy diam.
+        Praesent mauris ante, elementum et, bibendum at, posuere sit amet, nibh. Duis tincidunt lectus quis dui viverra vestibulum.
+        Suspendisse vulputate aliquam dui. Nulla elementum dui ut augue. Aliquam vehicula mi at mauris.
+        Maecenas placerat, nisl at consequat rhoncus, sem nunc gravida justo, quis eleifend arcu velit quis lacus.
+        Morbi magna magna, tincidunt a, mattis non, imperdiet vitae, tellus.
+        Sed odio est, auctor ac, sollicitudin in, consequat vitae, orci.
+        Fusce id felis. Vivamus sollicitudin metus eget eros.
+    </p>
 
-                <p>
-                    2.2 Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi.
-                    Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa. Sed eleifend nonummy diam.
-                    Praesent mauris ante, elementum et, bibendum at, posuere sit amet, nibh. Duis tincidunt lectus quis dui viverra vestibulum.
-                    Suspendisse vulputate aliquam dui. Nulla elementum dui ut augue. Aliquam vehicula mi at mauris.
-                    Maecenas placerat, nisl at consequat rhoncus, sem nunc gravida justo, quis eleifend arcu velit quis lacus.
-                    Morbi magna magna, tincidunt a, mattis non, imperdiet vitae, tellus.
-                    Sed odio est, auctor ac, sollicitudin in, consequat vitae, orci.
-                    Fusce id felis. Vivamus sollicitudin metus eget eros.
-                </p>
+    <p>
+        2.2 Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi.
+        Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa. Sed eleifend nonummy diam.
+        Praesent mauris ante, elementum et, bibendum at, posuere sit amet, nibh. Duis tincidunt lectus quis dui viverra vestibulum.
+        Suspendisse vulputate aliquam dui. Nulla elementum dui ut augue. Aliquam vehicula mi at mauris.
+        Maecenas placerat, nisl at consequat rhoncus, sem nunc gravida justo, quis eleifend arcu velit quis lacus.
+        Morbi magna magna, tincidunt a, mattis non, imperdiet vitae, tellus.
+        Sed odio est, auctor ac, sollicitudin in, consequat vitae, orci.
+        Fusce id felis. Vivamus sollicitudin metus eget eros.
+    </p>
 
-            </div>
+</div>
 
-            <div id='content-card' class='grid_5'>
+<div id='content-card' class='grid_5'>
 
-                <p>
-                    3. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi.
-                    Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa. Sed eleifend nonummy diam.
-                    Praesent mauris ante, elementum et, bibendum at, posuere sit amet, nibh. Duis tincidunt lectus quis dui viverra vestibulum.
-                    Suspendisse vulputate aliquam dui. Nulla elementum dui ut augue. Aliquam vehicula mi at mauris.
-                    Maecenas placerat, nisl at consequat rhoncus, sem nunc gravida justo, quis eleifend arcu velit quis lacus.
-                    Morbi magna magna, tincidunt a, mattis non, imperdiet vitae, tellus.
-                    Sed odio est, auctor ac, sollicitudin in, consequat vitae, orci.
-                    Fusce id felis. Vivamus sollicitudin metus eget eros.
-                </p>
+    <p>
+        3. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi.
+        Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa. Sed eleifend nonummy diam.
+        Praesent mauris ante, elementum et, bibendum at, posuere sit amet, nibh. Duis tincidunt lectus quis dui viverra vestibulum.
+        Suspendisse vulputate aliquam dui. Nulla elementum dui ut augue. Aliquam vehicula mi at mauris.
+        Maecenas placerat, nisl at consequat rhoncus, sem nunc gravida justo, quis eleifend arcu velit quis lacus.
+        Morbi magna magna, tincidunt a, mattis non, imperdiet vitae, tellus.
+        Sed odio est, auctor ac, sollicitudin in, consequat vitae, orci.
+        Fusce id felis. Vivamus sollicitudin metus eget eros.
+    </p>
 
-            </div>
+</div>
 
-            <?php require_once('include_scripts/footer.php') ?>
-        </div>
-
-    </body>
-</html>
+<?php require_once('includes/footer.php') ?>
