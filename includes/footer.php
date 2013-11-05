770f64de6d6675ef53b22debcb6f31a4e882e5cd
Pride Mokhele <pride.mokhele@gmail.com>
2013-11-05 11:14:41 +0200

    * slider setting is now in it's own script

--- includes/footer.php
+++ includes/footer.php
@@ -5,17 +5,12 @@
             </div>
         </div>
 
-            <script type="text/javascript" src="js/flexslider.js"></script>
-            <script type="text/javascript" charset="utf-8">
-                $(window).load(function() {
-                    $('.flexslider').flexslider({
-                        animation: "slide",
-                        easing: "string",
-                        slideshowSpeed: 7000,
-                        animationSpeed: 2500
-                    });
-                });
-            </script>
+            <?php
+                if($page_name == 'Home')
+                {
+                    require_once('includes/slider_setting.php');
+                }
+            ?>
 
     </body>
 </html>
---
+++ includes/slider_setting.php
@@ -0,0 +1,11 @@
+<script type="text/javascript" src="js/flexslider.js"></script>
+<script type="text/javascript" charset="utf-8">
+    $(window).load(function() {
+        $('.flexslider').flexslider({
+            animation: "slide",
+            easing: "string",
+            slideshowSpeed: 7000,
+            animationSpeed: 2500
+        });
+    });
+</script>
