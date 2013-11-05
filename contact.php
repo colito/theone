036e79c3060bed53bc022fbd53b483a94795b99d
Pride Mokhele <pride.mokhele@gmail.com>
2013-11-05 11:21:57 +0200

    * now using validation js from includes/validate_email.php

--- contact.php
+++ contact.php
@@ -65,42 +65,6 @@
     </form>
 </div>
 
-
-    <script type='text/javascript' src='js/validEmail.js'></script>
-    <script type='text/javascript'>
-
-        var $submit = $(".submit input");
-        var $required = $(".required");
-        function containsBlanks() {
-            var blanks = $required.map(function(){ return $(this).val() == "";});
-            return $.inArray(true, blanks) != -1;
-        }
-
-        function requiredFilledIn() {
-            if(containsBlanks() || !$("#email").validEmail())
-                $submit.attr("disabled","disabled");
-            else
-                $submit.removeAttr("disabled");
-        }
-
-        $("#form span").hide();
-        $("input,textarea").focus(function(){
-            $(this).next().fadeIn("slow");
-        }).blur(function(){
-                $(this).next().fadeOut("slow");
-            }).keyup(function(){
-                //Check all required fields.
-                requiredFilledIn();
-            });
-
-        $("#email").validEmail({on:"keyup", success:function(){
-            $(this).next().removeClass("error").addClass("valid");
-        }, failure:function(){
-            $(this).next().removeClass("valid").addClass("error");
-        }});
-
-        requiredFilledIn();
-    </script>
-
+<?php require_once('includes/validate_email.php'); ?>
 
 <?php require_once('includes/footer.php') ?>
