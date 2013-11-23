d378d0a83770880664e72e30971c8d1be81569d5
Pride Mokhele <pride.mokhele@gmail.com>
2013-11-23 18:19:30 +0200

    * changed all file paths to the ones defined in the config

--- includes/header.php
+++ includes/header.php
@@ -13,19 +13,19 @@ $config = new Config();
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable = no">
 
-    <link rel="stylesheet" href="../css/normalize.css" type="text/css" media="screen">
-    <link rel="stylesheet" href="../css/grid.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="<?php echo main_css?>normalize.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="<?php echo main_css?>grid.css" type="text/css" media="screen">
     <link href="http://fonts.googleapis.com/css?family=Armata" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="../css/flexslider.css" type="text/css" media="screen">
-    <link rel="stylesheet" href="../css/style.css" type='text/css' media="screen">
+    <link rel="stylesheet" href="<?php echo main_css?>flexslider.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="<?php echo main_css?>style.css" type='text/css' media="screen">
 
     <!-- GA -->
-    <script type='text/javascript'>
+    <script type="text/javascript">
         var _gaq = _gaq || [];
         _gaq.push(['_setAccount', 'UA-38880588-1']);
         _gaq.push(['_trackPageview']);
 
-        (function() {
+        (function() {navi_home
             var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
             ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
             var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
@@ -33,35 +33,35 @@ $config = new Config();
     </script>
 
     <!-- JQuery 2.0.3-->
-    <script type='text/javascript' src='../js/jquery-2.0.3.min.js'></script>
+    <script type="text/javascript" src="<?php echo main_jquery?>"></script>
 
 </head>
 
 <body oncontextmenu="return false">
 
-    <div id='topper'>
+    <div id="topper">
 
-        <div id='title'>
-            <h1><a href='../home'><?php echo $config->site_name; ?></a></h1>
+        <div id="title">
+            <h1><a href="<?php echo navi_home ?>"><?php echo $config->site_name; ?></a></h1>
         </div>
 
-        <div id='switch'></div>
+        <div id="switch"></div>
 
-        <div id='navigation'>
+        <div id="navigation">
             <ul>
-                <li id="<?php echo $home_active_page ?>" class='home'><a href='../home'>Home</a></li>
-                <li id="<?php echo $solutions_active_page ?>" class='solutions'><a href='../solutions'>Solutions</a></li>
-                <li id="<?php echo $contact_active_page ?>" class='contact'><a href='../contact'>Contact Us</a></li>
+                <li id="<?php echo $home_active_page ?>" class="home"><a href="<?php echo navi_home ?>">Home</a></li>
+                <li id="<?php echo $solutions_active_page ?>" class="solutions"><a href="<?php echo navi_solutions ?>">Solutions</a></li>
+                <li id="<?php echo $contact_active_page ?>" class="contact"><a href="<?php echo navi_contact ?>">Contact Us</a></li>
             </ul>
         </div>
 
     </div>
 
-    <div id='panel'>
+    <div id="panel">
         <ul>
-            <li id="<?php echo $home_active_page ?>" class='home'><a href='../home'>Home</a></li>
-            <li id="<?php echo $solutions_active_page ?>" class='solutions'><a href='../solutions'>Solutions</a></li>
-            <li id="<?php echo $contact_active_page ?>" class='contact'><a href='../contact'>Contact Us</a></li>
+            <li id="<?php echo $home_active_page ?>" class="home"><a href="<?php echo navi_home ?>">Home</a></li>
+            <li id="<?php echo $solutions_active_page ?>" class="solutions"><a href="<?php echo navi_solutions ?>">Solutions</a></li>
+            <li id="<?php echo $contact_active_page ?>" class="contact"><a href="<?php echo navi_contact ?>">Contact Us</a></li>
         </ul>
     </div>
 
