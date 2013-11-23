9ade67895d8252aeca6eb17c3753ebed59cc8de4
Pride Mokhele <pride.mokhele@gmail.com>
2013-11-23 18:05:06 +0200

    * defined navigation and included file paths

--- config.php
+++ config.php
@@ -9,4 +9,20 @@
         return $root;
     }
  }
+
+# navigation links
+define('navi_home', '../home');
+define('navi_solutions', '../solutions');
+define('navi_contact', '../contact');
+
+# included file paths
+define('main_include', '../includes/');
+define('main_header', '../includes/header.php');
+define('main_footer', '../includes/footer.php');
+define('main_topper', '../includes/topper.php');
+
+define('main_images', '../images');
+define('main_css', '../css/');
+define('main_js', '../js/');
+
 ?>
