4c625d58bcac45a81ac18f08f32c6528e56aed1e
Pride Mokhele <pride.mokhele@gmail.com>
2013-11-23 14:43:49 +0200

    * omitted grid container

--- includes/slider.php
+++ includes/slider.php
@@ -4,16 +4,15 @@
     $height = '';
 ?>
 
-<div id='picshow' class='grid_12 flexslider omega'>
-    <ul class='slides'>
-        <li>
-            <img src='images/1.jpg' width="<?php echo $width?>" height="<?php echo $height;?>">
-        </li>
-        <li>
-            <img src='images/2.jpg' width="<?php echo $width?>" height="<?php echo $height;?>">
-        </li>
-        <li>
-            <img src='images/3.jpg' width="<?php echo $width?>" height="<?php echo $height;?>">
-        </li>
-    </ul>
-</div>
+
+ <ul class='slides'>
+    <li>
+         <img src='images/1.jpg' width="<?php echo $width?>" height="<?php echo $height;?>">
+    </li>
+    <li>
+         <img src='images/2.jpg' width="<?php echo $width?>" height="<?php echo $height;?>">
+    </li>
+    <li>
+         <img src='images/3.jpg' width="<?php echo $width?>" height="<?php echo $height;?>">
+    </li>
+ </ul>
