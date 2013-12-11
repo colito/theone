97e9afb2fe020baa7318391964fbedcdb7222a40
Pride Mokhele <pride.mokhele@gmail.com>
2013-12-11 13:37:31 +0200

    * added construction page

---
+++ construction/index.php
@@ -0,0 +1,51 @@
+<?php
+    //$dir_level = '../'
+?>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable = no">
+
+    <title>SSM Under Construction</title>
+    <link rel="stylesheet" href="<?php echo $dir_level?>css/normalize.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="<?php echo $dir_level?>css/grid.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="<?php echo $dir_level?>css/rotate.css" type="text/css" media="screen" />
+
+    <script type="text/javascript">
+
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-38880588-1']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+
+    </script>
+
+</head>
+<body>
+<div class="container clearfix">
+
+    <div id="header" class="grid_12">
+        <h1>Success Motivation</h1>
+    </div>
+
+    <div id="gear" class="grid_12">
+        <img src="<?php echo $dir_level?>img/cog-cogwheel-gear-zahnrad-md.png" alt="rotating gear" />
+    </div>
+
+    <div id="par" class="grid_12">
+        <p>
+            The Success Motivation official site is currently undergoing
+            construction.
+        </p>
+        <p>
+            Launch date to be announced soon...
+        </p>
+    </div>
+</div>
+</body>
+</html>
